79aa548e70d742c26e4bf08a5510d912eb417f3a
Yosvany <yosvany@yosvanyblanco.com>
2019-01-30 23:23:40 -0600

    added helper for heapify and added heapsort. Also added main method for testing class

--- Shared_Code/src/MaxHeapArray.java
+++ Shared_Code/src/MaxHeapArray.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MaxHeapArray<T extends Comparable<T>> implements Heap<T> {
 
@@ -7,11 +9,75 @@ public class MaxHeapArray<T extends Comparable<T>> implements Heap<T> {
 
     public MaxHeapArray() {
         myHeapArray = new ArrayList<>();
+        myHeapArray.add(null);
         myHeapSize = 0;
     }
 
-    public void heapify() {
+    public MaxHeapArray(ArrayList<T> input) {
+        myHeapArray = new ArrayList<>();
+        myHeapArray.add(null);
+        myHeapArray.addAll(input);
+        myHeapSize = input.size();
+    }
 
+    public MaxHeapArray(T[] input) {
+        myHeapArray = new ArrayList<>();
+        myHeapArray.add(null);
+        myHeapArray.addAll(Arrays.asList(input));
+        myHeapSize = input.length;
+    }
+
+    public void heapify() {
+        for (int i = myHeapSize / 2; i >= 1; i--) {
+            heapifyHelp(i);
+        }
+    }
+
+    public void heapSort() {
+        int rememberHeapSize = myHeapSize;
+        for (int i = myHeapSize; i > 0; i--) {
+            heapify();
+            Collections.swap(myHeapArray, 1, myHeapSize);
+            myHeapSize--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] tempArray = new Integer[]{4,7,5,2,7,3};
+        MaxHeapArray<Integer> maxHeapArray = new MaxHeapArray<>(tempArray);
+        System.out.println(maxHeapArray);
+        maxHeapArray.heapify();
+        System.out.println(maxHeapArray);
+        maxHeapArray.heapSort();
+        System.out.println(maxHeapArray);
+    }
+
+    private void heapifyHelp(int i) throws IllegalArgumentException {
+        if (checkIfNegativeOrZero(i)) {
+            throw new IllegalArgumentException("Only Positive integers are allowed.");
+        }
+        int left = i << 1;
+        int right = (i << 1) + 1;
+        int largest = i;
+        if (left <= myHeapSize && myHeapArray.get(left).compareTo(myHeapArray.get(largest)) > 0) {
+            largest = left;
+        }
+        if (right <= myHeapSize && myHeapArray.get(right).compareTo(myHeapArray.get(largest)) > 0) {
+            largest = right;
+        }
+        if (largest != i) {
+            Collections.swap(myHeapArray, largest, i);
+            heapifyHelp(largest);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (T item: myHeapArray) {
+            output.append(item).append(" ");
+        }
+        return output.toString();
     }
 
     public void replaceRoot(T value) {
@@ -23,7 +89,7 @@ public class MaxHeapArray<T extends Comparable<T>> implements Heap<T> {
     }
 
     public T peak() {
-        return this.myHeapArray.get(1);
+        return myHeapArray.get(1);
     }
 
     public T pop() {
@@ -31,6 +97,10 @@ public class MaxHeapArray<T extends Comparable<T>> implements Heap<T> {
     }
 
     public int size() {
-        return this.myHeapSize;
+        return myHeapSize;
+    }
+
+    private boolean checkIfNegativeOrZero(int toCheck) {
+        return toCheck < 1;
     }
 }
